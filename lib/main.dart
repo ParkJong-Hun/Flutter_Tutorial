aa01c7f17d55f912bd79f130cc23d63226388115
ParkJong-Hun <thesn515@gmail.com>
2021-10-20 18:06:38 +0900

    2021/10/20 18:06 Navigator, ElevatedButton

--- lib/main.dart
+++ lib/main.dart
@@ -13,47 +13,49 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.red
       ),
-      home: MyPage(),
+      home: FirstPage(),
     );
   }
 }
 
-class MyPage extends StatelessWidget {
-  const MyPage({Key? key}) : super(key: key);
+class FirstPage extends StatelessWidget {
+  const FirstPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final ButtonStyle buttonStyle = TextButton.styleFrom(
-      minimumSize: Size(60, 30),
-      backgroundColor: Colors.red,
-    );
-
     return Scaffold(
       appBar: AppBar(
-        title: Text('Snack Bar'),
+        title: Text('First page'),
         centerTitle: true,
       ),
-      body: Builder(builder: (BuildContext ctx) {//위젯의 BuildContext를 갖는 부모-자식 관계의 자식 위젯을 리턴.
-        return Center(
-          child: TextButton(//원래 FlatButton이였던 Text가 쓰여져 있는 일반적인 버튼 위젯.
-            //눌렀을 때 기능 구현.
-            onPressed: (){
-            //현재 주어진 context에서 위로 올라가면서 가장 가까운 Scaffold를 반환. (단 Builder가 없을 경우, Scaffold가 어디에 있는지 찾을 수 없을 수 있음)
-            ScaffoldMessenger.of(ctx).showSnackBar(SnackBar(//ToastText 같은 위젯. 화면 하단부에 출력됨.
-              //표시될 내용.
-              content: Text('Hello'),));
-            },
-            //버튼에 쓰여질 Text 위젯을 인자로 사용.
-            child: Text('Show me',
-              //ButtonStyle을 인자로 사용. 버튼의 내용을 꾸밈.
-              style: TextStyle(
-                color: Colors.white,
-              ),
-            ),
-              style: buttonStyle),
-        );
-      },)
+      body: Center(
+        child: ElevatedButton(child: Text('Go to the Second page'), onPressed: () {
+          Navigator.push(context,
+            MaterialPageRoute(
+              builder: (_) => SecondPage()
+            )
+          );
+        },),
+      ),
     );
   }
 }
 
+class SecondPage extends StatelessWidget {
+  const SecondPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Second page'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: ElevatedButton(child: Text('Go to the First page'), onPressed: () {
+          Navigator.pop(context);
+        },),
+      ),
+    );
+  }
+}
